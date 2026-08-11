e5f8c13a691b61f8a6e67d7e7c646638c8bdf467
Cody Gunton <codygunton@gmail.com>
2026-08-11 16:59:14 -0400

    Use native Zesu IO interface

--- src/zkvm/ssz_decode_native_root.zig
+++ src/zkvm/ssz_decode_native_root.zig
@@ -9,7 +9,10 @@ const zkvm_io = @import("zkvm_io");
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
     zesu_allocator.set(allocator);
-    const encoded = try zkvm_io.read_input(allocator);
+    var input_ptr: [*]const u8 = undefined;
+    var input_size: usize = 0;
+    zkvm_io.read_input(&input_ptr, &input_size);
+    const encoded = input_ptr[0..input_size];
     const decoded = ssz_decode.decode(allocator, encoded) catch {
         observation.writeFailure();
         return;
